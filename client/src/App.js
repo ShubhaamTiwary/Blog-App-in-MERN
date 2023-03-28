67c0c47d8c540a721177616bd513d4c34edba7c4
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-03-28 14:56:14 +0530

    Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,19 @@
 import './App.css';
+import { Post } from './Post';
+import {Route, Routes} from 'react-router-dom';
+import Layout from './Layout';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+
+
 function App() {
   return (
-    <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-            <a href="">Login</a>
-            <a href="">Register</a>
-        </nav>
-      </header>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png" alt="">
-          </img>
-        </div>
-        <div className="texts">
-        <h2>kasbchjbj zclivz zj zjkbvbjkz mz  vhjbhjazjv jabuisvask vjsabuifnjia </h2>
-        < p className="info">
-          <span className="author">Shubhaam Tiwary</span>
-        </p>
-        <p>jhscac  gjasuguyasbubvyubaydyubyusagu h A UYUGDYUYUBSAB UQUYABJB</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png" alt=""></img>
-        </div>
-        <div className="texts">
-        <h2>kasbchjbj zclivz zj zjkbvbjkz mz  vhjbhjazjv jabuisvask vjsabuifnjia </h2>
-        <p>jhscac  gjasuguyasbubvyubaydyubyusagu h A UYUGDYUYUBSAB UQUYABJB</p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png" alt=""></img>
-        </div>
-        <div className="texts">
-        <h2>kasbchjbj zclivz zj zjkbvbjkz mz  vhjbhjazjv jabuisvask vjsabuifnjia </h2>
-        <p>jhscac  gjasuguyasbubvyubaydyubyusagu h A UYUGDYUYUBSAB UQUYABJB</p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path='/' element={<Layout />} >
+        <Route index element={ <IndexPage /> } />
+        <Route path='/login' element={<LoginPage /> } /> 
+      </Route>
+    </Routes>
   );
 }
 
---
+++ client/src/Post.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Post = () => {
+  return (
+    <div className="post">
+        <div className="image">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1280px-Image_created_with_a_mobile_phone.png" alt="">
+          </img>
+        </div>
+        <div className="texts">
+          <h2>kasbchjbj zclivz zj zjkbvbjkz mz  vhjbhjazjv jabuisvask vjsabuifnjia </h2>
+          <p className="info">
+            <a className="author">Shubhaam Tiwary</a>
+            <time>2023-03-28  15:00</time>
+          </p>
+          <p className="summary">jhscac gjasuguyasbubvyubaydyubyusagu h A UYUGDYUYUBSAB UQUYABJB</p>
+        </div>
+    </div>
+  )
+}
