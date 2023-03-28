a6be145325b2554b23fa220cf8a1af729124debd
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-03-28 16:32:07 +0530

    login & register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import { Post } from './Post';
 import {Route, Routes} from 'react-router-dom';
 import Layout from './Layout';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
       <Route path='/' element={<Layout />} >
         <Route index element={ <IndexPage /> } />
         <Route path='/login' element={<LoginPage /> } /> 
+        <Route path='/register' element={<RegisterPage /> } /> 
       </Route>
     </Routes>
   );
