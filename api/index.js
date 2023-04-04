aec336e73fa774ec58477c15a5a65c9e06891c21
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-04-05 02:21:40 +0530

    implemented single page post

--- api/index.js
+++ api/index.js
@@ -97,6 +97,13 @@ app.get('/post',async (req,res)=>{
 });
 
 
+app.get('/post/:id', async (req, res) => {
+  const {id} = req.params;
+  const postDoc = await Post.findById(id).populate('author', ['username']);
+  res.json(postDoc);
+})
+
+
 app.listen(4000);
 
 // HfWBicaV6Z3xdLw7
